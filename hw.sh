0633cfda10572deebbb04f3c125e080191366273
woodrock <j.r.h.wood98@gmail.com>
2020-09-08 15:01:59 +1200

    Add missing databases from wms to the setup file

---
+++ hw.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# This script is a sanity check that the cgi-bin is configured. 
+# It should display a simple html page at the following URL: 
+# http://wellmapsrvdev02.niwa.local/cgi-bin/hw.sh
+
+echo "Content-type: text/html\n\n";
+
+echo "<h1>Hello World!</h1>";
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 createdb -T template0 template1;
 
 # The Databses that need to be created
-declare -a DATABASES=("test" "ecovis" "trawl")
+declare -a DATABASES=("acoustic" "basemap" "bioregions" "bio" "bio_atlas" "chat-call" "cli_map" "corax2005" "nzoa" "nzoi" "nzpolitical" "os2020_oceans" "test" "test_mafbnz" "trawl")
 
 # Create each database in the list of databases
 for DATABASE in ${DATABASES[@]}; do
